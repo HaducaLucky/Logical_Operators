ff56bd2c87b5d1cdb6a44038e8ba44211d93c88f
HaducaLucky <147017329+HaducaLucky@users.noreply.github.com>
2024-03-27 14:22:32 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+// logical operators = used to combine or manipulate boolean values
+//                     (true or false)
+
+//                     AND = &&
+//                     OR = ||
+//                     NOT = !
+
+// const temp = 1;
+
+// if(temp > 0 && temp <= 30){
+//     console.log("The weather is GOOD");
+// }
+// else{
+//     console.log("The weather is BAD")
+// }
+
+// const temp = 1;
+
+// if(temp <= 0 || temp > 30){
+//     console.log("The weather is BAD");
+// }
+// else{
+//     console.log("The weather is GOOD");
+// }
+
+const isSunny = false;
+
+if(!isSunny){
+    console.log("It is CLOUDY");
+}
+else{
+    console.log("It is SUNNY")
+}
